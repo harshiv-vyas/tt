7f02449b7a9b5ba0f705f5e6dc63a9e54eb77b5b
harshiv-vyas <hrvyas.2006@gmail.com>
2024-01-02 12:13:07 +0530

    aldja

---
+++ hp.c
@@ -0,0 +1,30 @@
+/*#include <stdio.h>
+
+int main()
+{
+    char ty ='a';
+    do{
+        printf("%c\n",ty);
+        ty+=4;
+    }while(ty<='z');
+    
+
+    return 0;
+}*/
+
+
+/*#include <stdio.h>
+
+int main()
+{
+    int num=20;
+    int k=5;
+    int sum;
+    for(int i=1 ; i<num ; i++){
+        if(i%k==0){
+            sum+=i;
+        }
+    }
+    printf("%d\n",sum);
+    return 0;
+}*/
